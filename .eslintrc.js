b022456a6e1e90b5d7af12eea5cf4d207f14cb45
Justin Waite <justindwaite@gmail.com>
2023-04-15 12:40:31 -0600

    chore: initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+/** @type {import("@types/eslint").Linter.BaseConfig} */
+module.exports = {
+  extends: [
+    "@remix-run/eslint-config",
+    "@remix-run/eslint-config/node",
+    "@remix-run/eslint-config/jest-testing-library",
+    "prettier"
+  ],
+  // overrides for playwright directory
+  overrides: [
+    {
+      files: ["playwright/**/*"],
+      extends: [
+        "plugin:playwright/playwright-test",
+        "prettier"
+      ]
+    }
+  ],
+  // We're using vitest which has a very similar API to jest
+  // (so the linting plugins work nicely), but we have to
+  // set the jest version explicitly.
+  settings: {
+    jest: {
+      version: 28
+    }
+  }
+};
